68abf4e77ad1c1cda82c19577b6364d498fbaaf1
Peter Hunt <peter.s.hunt@gmail.com>
2017-10-09 22:17:11 +0000

    add http basic auth to admin controllers

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,6 @@
 class Admin::CategoriesController < ApplicationController
 
+  http_basic_authenticate_with name: "Jungle", password: "book"
   #before_filter :authorize
 
   def index
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: "Jungle", password: "book"
   #before_filter :authorize
 
   def index
